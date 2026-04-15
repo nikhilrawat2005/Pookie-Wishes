102b7a700ed5989b1e55f03bd468854288d68045
Nikhil Rawat <nikhil2005114@gmail.com>
2026-04-16 00:11:34 +0530

    form changes

--- templates/anniversary-journey-template/script.js
+++ templates/anniversary-journey-template/script.js
@@ -384,3 +384,9 @@ function showEnding() {
 function restartJourney() {
     location.reload();
 }
+
+document.addEventListener('DOMContentLoaded', async () => {
+    await initData();
+    buildPhotoMoments();
+    initPetalCanvas();
+});
